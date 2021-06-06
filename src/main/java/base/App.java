f53803f1fc661e15a54f1a131f7e7bd2f19ccdb5
SoroushAli <84461814+SoroushAli@users.noreply.github.com>
2021-06-06 12:58:51 -0400

    Solution for exercise 18

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -3,6 +3,9 @@
  *  Copyright 2021 Soroush Alinaghian
  */
 package base;
+
+import java.util.Scanner;
+
 /*
 Create a program that converts temperatures from Fahrenheit to Celsius or from Celsius to Fahrenheit. Prompt for the starting temperature. The program should prompt for the type of conversion and then perform the conversion.
 
@@ -32,8 +35,66 @@ Break the program into functions that perform the computations.
 Implement this program as a GUI program that automatically updates the values when any value changes.
 Modify the program so it also supports the Kelvin scale.
  */
-public class App {
-    public static void main(String[] args) {
+public class App<scale> {
+    static Scanner in = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        String temperature = pickTempScale();
+        char temp_choice = celsiusOrFahrenheit(temperature);
+        conversion(temp_choice);
+    }
+
+    private static void conversion(int temp_choice) {
+        if(temp_choice == 'c'||temp_choice == 'C'){
+            System.out.print("Please enter the temperature in Fahrenheit: ");
+            int f_degrees = getFahrenheit();
+            fahrenheitToCelsius(f_degrees);
+        }else if(temp_choice == 'f' ||temp_choice == 'F'){
+            System.out.print("Please enter the temperature in Celsius: ");
+            int c_degrees = getCelsius();
+            celsiusToFahrenheit(c_degrees);
+        }
+    }
+
+    private static void celsiusToFahrenheit(int degrees) {
+        int f = (degrees * 9/5) + 32;
+        System.out.println("The temperature in fahrenheit is " + f + ".");
+    }
+
+    private static int getCelsius() {
+        return in.nextInt();
+    }
+
+    private static int getFahrenheit() {
+        return in.nextInt();
+    }
+
+    private static void fahrenheitToCelsius(int degrees) {
+        int c = (degrees - 32) * 5/9;
+        System.out.println("The temperature in celsius is " + c + ".");
+
+    }
+
+    private static char celsiusOrFahrenheit(String temp) {
+
+        char scale = temp.charAt(0);
+        char ch1, ch2;
+
+        if(scale == 'c' || scale == 'C') {
+            ch1 = Character.toUpperCase(scale);
+            System.out.print("Your choice: " );
+            System.out.println(ch1);
+        }else if(scale == 'f' || scale == 'F'){
+            ch2 = Character.toUpperCase(scale);
+            System.out.print("Your choice: ");
+            System.out.println(ch2);
+        }
+        return scale;
+    }
+
+    private static String pickTempScale() {
+        System.out.println("Press C to convert from Fahrenheit to Celsius.");
+        System.out.println("Press F to convert from Celsius to Fahrenheit.");
+        return in.next();
     }
 }
